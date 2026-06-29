ad8c96cdca367ece042122cad63e93baa5f1fd14
TheHardGamer <gamelovr695@gmail.com>
2026-06-29 23:03:31 +0000

    [1/4] Introduce a native backup/restore solution
    Co-authored-by: Dmitrii bankersenator@gmail.com
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -127,6 +127,7 @@ import com.android.server.adb.AdbService;
 import com.android.server.alarm.AlarmManagerService;
 import com.android.server.am.ActivityManagerService;
 import com.android.server.ambientcontext.AmbientContextManagerService;
+import com.android.server.appbackup.AppDataBackupService;
 import com.android.server.app.GameManagerService;
 import com.android.server.appbinding.AppBindingService;
 import com.android.server.appfunctions.AppFunctionManagerService;
@@ -2850,6 +2851,10 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("FreeformService");
             mSystemServiceManager.startService(FreeformService.class);
+            t.traceEnd();
+
+            t.traceBegin("AppDataBackupService");
+            mSystemServiceManager.startService(AppDataBackupService.class);
             t.traceEnd();
 
             if (!isWatch) {
